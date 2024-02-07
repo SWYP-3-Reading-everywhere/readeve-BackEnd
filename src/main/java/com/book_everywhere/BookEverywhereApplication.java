1f583bf0076e24cc3080e361df9cd5f7b6b639e0
dltjdgh0428 <dltjdgh0428@naver.com>
2024-02-07 16:17:39 +0900

    git connection check

--- src/main/java/com/book_everywhere/BookEverywhereApplication.java
+++ src/main/java/com/book_everywhere/BookEverywhereApplication.java
@@ -8,6 +8,7 @@ public class BookEverywhereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookEverywhereApplication.class, args);
+		System.out.println("연동 테스트입니다.");
 	}
 
 }
