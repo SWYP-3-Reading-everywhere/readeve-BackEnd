85d9e65c48e25bf6dc65ccc9562427b08b4b4d3d
unknown <rnjsrleo1@gmail.com>
2024-02-07 16:11:58 +0900

    test: hello world

--- src/main/java/com/book_everywhere/BookEverywhereApplication.java
+++ src/main/java/com/book_everywhere/BookEverywhereApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookEverywhereApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(BookEverywhereApplication.class, args);
 	}
 
