5da373be2fc58af7e135a65dc791fd670c129510
unknown <rnjsrleo1@gmail.com>
2024-02-07 16:33:37 +0900

    test : test request
    hello world 지우기

--- src/main/java/com/book_everywhere/BookEverywhereApplication.java
+++ src/main/java/com/book_everywhere/BookEverywhereApplication.java
@@ -8,7 +8,6 @@ public class BookEverywhereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookEverywhereApplication.class, args);
-		System.out.println("helloWorld");
 	}
 
 }
