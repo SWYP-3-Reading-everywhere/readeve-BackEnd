d99b16e23a643b429c5d218cd4896415b896d844
dltjdgh0428 <dltjdgh0428@naver.com>
2024-02-07 14:35:16 +0900

    BackEnd project start!!!

---
+++ src/main/java/com/book_everywhere/BookEverywhereApplication.java
@@ -0,0 +1,13 @@
+package com.book_everywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEverywhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book_everywhere/BookEverywhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_everywhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEverywhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
