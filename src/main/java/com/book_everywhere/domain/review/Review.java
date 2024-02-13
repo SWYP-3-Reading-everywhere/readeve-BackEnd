6dac27709d5cb3909bf6c049b2ae5538c7511cc4
dltjdgh0428 <dltjdgh0428@naver.com>
2024-02-14 00:19:57 +0900

    test: gitignore 확인

--- src/main/java/com/book_everywhere/domain/review/Review.java
+++ src/main/java/com/book_everywhere/domain/review/Review.java
@@ -1,9 +1,12 @@
 package com.book_everywhere.domain.review;
 
+import com.book_everywhere.domain.pin.Pin;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -16,4 +19,8 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "pin_id") // 데이터베이스 내의 컬럼 이름 지정
+    private Pin pin; // Pin 엔티티와의 관계를 나타냄
 }
