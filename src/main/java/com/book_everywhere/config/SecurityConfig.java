8ac67d433924b87c21e688bfd2d2e4501678436f
dltjdgh0428 <dltjdgh0428@naver.com>
2024-02-21 13:39:09 +0900

    feat: 로그인 인증에 따른 Front Get요청 처리
    현재 hasRole=MEMBER시 CORS오류가 생성됩니다. 잠시 permitAll처리 해두었습니다

--- src/main/java/com/book_everywhere/config/SecurityConfig.java
+++ src/main/java/com/book_everywhere/config/SecurityConfig.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -27,21 +28,38 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // CORS 필터를 위한 Bean 설정
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // 모든 출처 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
 
         http
+                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/login")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(introspector, "/")).hasRole("MEMBER")
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/api/**")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2Login ->
-                        oauth2Login.userInfoEndpoint(userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(customOAuth2UserService)))
+                        oauth2Login
+                                .userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig.userService(customOAuth2UserService))
+                                .successHandler((request, response, authentication) -> {
+                                    // 로그인 성공 후 리다이렉션할 URL 지정
+                                    response.sendRedirect("http://localhost:3000");
+                                }))
         ;
         return http.build();
     }
