44bc06f3fda61ece5020ac364c1fcdb3cb1ec13d
kf3907 <rnjsrleo1@gmail.com>
2024-02-27 01:19:51 +0900

    feat/test : 모든 태그의 개수 세기 기능 구현/테스트 완료

--- src/main/java/com/book_everywhere/web/TagController.java
+++ src/main/java/com/book_everywhere/web/TagController.java
@@ -2,9 +2,11 @@ package com.book_everywhere.web;
 
 import com.book_everywhere.service.TagService;
 import com.book_everywhere.web.dto.CMRespDto;
+import com.book_everywhere.web.dto.tag.TagCountDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +18,13 @@ public class TagController {
     private final TagService tagService;
     @GetMapping("/api/tags")
     public CMRespDto<?> findAllTag() {
-        List<String> 모든태그조회 = tagService.모든태그조회();
-        return new CMRespDto<>(HttpStatus.OK, 모든태그조회, "태그 조회 성공");
+        List<String> result = tagService.모든태그조회();
+        return new CMRespDto<>(HttpStatus.OK, result, "태그 조회 성공");
+    }
+
+    @GetMapping("/api/tags/count")
+    public CMRespDto<?> findTagCount() {
+        List<TagCountDto> result = tagService.모든태그의개수조회();
+        return new CMRespDto<>(HttpStatus.OK, result, "태그 개수 조회 성공");
     }
 }
