849d0f4afda94b357c442ebb17054302185b5daf
kf3907 <rnjsrleo1@gmail.com>
2024-03-06 01:19:44 +0900

    fix : 누락된 대괄호 추가

--- src/main/java/com/book_everywhere/web/DataController.java
+++ src/main/java/com/book_everywhere/web/DataController.java
@@ -25,14 +25,14 @@ public class DataController {
         return new CMRespDto<>(HttpStatus.OK, result,"모든 데이터 조회 성공");
     }
 
-    @GetMapping("/api/data/all/userId")
+    @GetMapping("/api/data/all/{userId}")
     public CMRespDto<?> findAllData(@PathVariable Long userId) {
         List<AllDataDto> result = dataService.유저독후감조회(userId);
         return new CMRespDto<>(HttpStatus.OK, result,"모든 데이터 조회 성공");
     }
 
 
-    @GetMapping("/api/tags/pinId")
+    @GetMapping("/api/tags/{pinId}")
     public CMRespDto<?> findTagsInPin(@PathVariable Long pinId) {
         List<Tagged> result = dataService.findAllTaggedList(pinId);
         return new CMRespDto<>(HttpStatus.OK, result, "핀에 대한 모든 태그 조회 성공");
