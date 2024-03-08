4903b47f63b0e2960be0ac8c4b748a62fae96e05
kf3907 <rnjsrleo1@gmail.com>
2024-03-08 13:06:50 +0900

    feat : category 객체 추가

---
+++ src/main/java/com/book_everywhere/domain/category/Category.java
@@ -0,0 +1,27 @@
+package com.book_everywhere.domain.category;
+
+import com.book_everywhere.domain.tag.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "category")
+    private List<Tag> tag;
+
+    private String name;
+}
