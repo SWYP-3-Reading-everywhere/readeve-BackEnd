cb5308799e6c0b7596b59189fdf9cf16b6d75bb3
kf3907 <rnjsrleo1@gmail.com>
2024-03-15 16:52:21 +0900

    fix : exception handler 테스트
    exception handler를 재설정하여 테스트합니다.

--- src/main/java/com/book_everywhere/web/dto/CMRespDto.java
+++ src/main/java/com/book_everywhere/web/dto/CMRespDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Data
 @AllArgsConstructor
