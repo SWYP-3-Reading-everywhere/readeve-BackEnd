cf1aa5711ee9d23b6d1dbb1c6e400bea72cb1cf3
kf3907 <rnjsrleo1@gmail.com>
2024-03-16 19:05:22 +0900

    fix : 수정 오류 해결

--- src/main/java/com/book_everywhere/domain/review/Review.java
+++ src/main/java/com/book_everywhere/domain/review/Review.java
@@ -17,7 +17,6 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @Entity
 @Getter
-@Setter
 public class Review {
 
     @Id
@@ -47,21 +46,21 @@ public class Review {
     private Timestamp updateAt;
 
   //==연관 관계 편의 메서드==//
-//    public void setBook(Book book) {
-//        if(this.book != null) {
-//            this.book.getReviews().remove(this);
-//        }
-//        this.book = book;
-//        book.getReviews().add(this);
-//    }
-//
-//    public void setPin(Pin pin) {
-//        if(this.pin != null) {
-//            this.pin.getReviews().remove(this);
-//        }
-//        this.pin = pin;
-//        pin.getReviews().add(this);
-//    }
+    public void setBook(Book book) {
+        if(this.book != null) {
+            this.book.getReviews().remove(this);
+        }
+        this.book = book;
+        book.getReviews().add(this);
+    }
+
+    public void setPin(Pin pin) {
+        if(this.pin != null) {
+            this.pin.getReviews().remove(this);
+        }
+        this.pin = pin;
+        pin.getReviews().add(this);
+    }
 
     //==수정 메서드==//
     public void changeReview(String title, String content, boolean isPrivate, String writer) {
