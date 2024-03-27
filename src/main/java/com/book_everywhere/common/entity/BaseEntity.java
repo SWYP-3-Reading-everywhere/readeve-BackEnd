1a177d76c162b465aa5aafbf1a9b3d9262fffc90
dltjdgh0428 <dltjdgh0428@naver.com>
2024-03-27 23:51:03 +0900

    refactoring : toDto를 통한 서비스로직 단순화

--- src/main/java/com/book_everywhere/common/entity/BaseEntity.java
+++ src/main/java/com/book_everywhere/common/entity/BaseEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity{
+public class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
     @Column(updatable = false)
